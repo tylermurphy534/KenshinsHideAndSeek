b22fad20d2eff2b59f376ff001eee7d8646593df
bobby29831 <bobby29831@gmail.com>
2022-05-12 20:10:55 -0500

    refactor: remove more unused imports

--- src/main/java/net/tylermurphy/hideAndSeek/Main.java
+++ src/main/java/net/tylermurphy/hideAndSeek/Main.java
@@ -27,25 +27,17 @@ import net.tylermurphy.hideAndSeek.game.Board;
 import net.tylermurphy.hideAndSeek.game.CommandHandler;
 import net.tylermurphy.hideAndSeek.game.EventListener;
 import net.tylermurphy.hideAndSeek.game.Game;
-import net.tylermurphy.hideAndSeek.util.Status;
 import net.tylermurphy.hideAndSeek.util.TabCompleter;
 import net.tylermurphy.hideAndSeek.util.UUIDFetcher;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.util.List;
-
-import static net.tylermurphy.hideAndSeek.configuration.Config.errorPrefix;
-import static net.tylermurphy.hideAndSeek.configuration.Localization.message;
 
 public class Main extends JavaPlugin implements Listener {
 	
